4a1d7c55ec8437d96ec131376552052d2541dda9
medduge50 <69568693+medduge50@users.noreply.github.com>
2021-09-13 18:20:20 +0900

    Update botclient.py

--- botclient.py
+++ botclient.py
@@ -1,4 +1,4 @@
-import discord, datetime, pytz, os
+import discord, datetime, pytz, os, string, random
 
 ###################설정 하는 곳###################
 token = os.environ["BOT_TOKEN"]
@@ -82,8 +82,8 @@ async def on_message(message):
                 content = message.content[4:]
                 channeid,soure=content.split("&")
             except:
-                embed3 = discord.Embed(title=f'{name}', description=f'채널 아이디와 전달할 매세지를 보내주세요.', color=0xFF0000)
-                await message.channel.send(embed=embed3)                   
+                embed3 = discord.Embed(title=f'{name}', description=f'채널 아이디와 공지할 내용을 보내주세요.', color=0xFF0000)
+                return await message.channel.send(embed=embed3)                   
             target = discord.utils.get(message.guild.roles, name=f"{role}") 
             if not target in message.author.roles: 
                 return
@@ -106,16 +106,16 @@ async def on_message(message):
                     embed = discord.Embed(title=f'{name} NOTICE', description=f'{soure}', timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x0000FF)    
                     embed.set_footer(text=footer)
                     embed.set_thumbnail(url=icon)  
-                    await channel.send(embed=embed)   
-                else:            
+                    return await channel.send(embed=embed)   
+                else:             
                     if msg.content == "네":                          
                         channel = client.get_channel(int(channeid)) 
                         embed = discord.Embed(title=f'{name} NOTICE', description=f'{soure}', timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x0000FF)    
                         embed.set_footer(text=footer)
                         embed.set_thumbnail(url=icon)  
-                        await channel.send(f"@everyone" ,embed=embed)      
+                        return await channel.send(f"@everyone" ,embed=embed)      
                     else:
-                        embed3 = discord.Embed(title=f'{name} NOTICE', description=f'네/아니요를 보내주세요. 전달이 취소 되었습니다.', color=0xFF0000)
+                        embed3 = discord.Embed(title=f'{name} NOTICE', description=f'네/아니요를 보내주세요. 공지가 취소 되었습니다.', color=0xFF0000)
                         return await message.channel.send(embed=embed3)                                            
 
         except Exception as errorcode:
@@ -127,19 +127,39 @@ async def on_message(message):
             embed2.add_field(name="오류 코드", value=errorcode, inline=False)  
             await error.send(embed=embed2)
             embed3 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-            await message.channel.send(embed=embed3)    
+            return await message.channel.send(embed=embed3)    
 
     if message.content.startswith("!인증"):
         try:
             if message.author.bot:
                 return
             else:
-                embed = discord.Embed(timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x0000FF)    
-                embed.add_field(name=f"{name} 인증 시스템", value=f"{message.author}님 감사합니다 ! 성공적으로 인증되셨습니다 !\n시민 권한이 지급 됩니다.", inline=False)  
-                embed.set_footer(text=footer)
-                embed.set_thumbnail(url=icon)  
-                await message.channel.send(message.author.mention ,embed=embed)
-                return await message.author.add_roles(discord.utils.get(message.guild.roles, id=int(dlswmdrole)))
+                _LENGTH = 6
+                string_pool = string.ascii_letters + string.digits
+                result = "" 
+                for i in range(_LENGTH) :
+                    result += random.choice(string_pool)
+                embed3 = discord.Embed(title=f'{name} 인증 시스템', description=f'30초안에 **{result}**를 보내주세요.', color=0x0000FF)
+                await message.channel.send(embed=embed3)          
+
+                def check(msg):
+                    return msg.author == message.author and msg.channel == message.channel 
+
+                try:
+                    msg = await client.wait_for("message", timeout=30, check=check) 
+                except:
+                    embed3 = discord.Embed(title=f'{name} 인증 시스템', description=f'30초 초과로 인하여 인증 시스템이 종료되었습니다. 다시 시도해주세요.', color=0xFF0000)
+                    return await message.channel.send(embed=embed3)    
+                if msg.content == result:              
+                    embed = discord.Embed(timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x0000FF)    
+                    embed.add_field(name=f"{name} 인증 시스템", value=f"{message.author}님 감사합니다 ! 성공적으로 인증되셨습니다 !\n시민 권한이 지급 됩니다.", inline=False)  
+                    embed.set_footer(text=footer)
+                    embed.set_thumbnail(url=icon)  
+                    await message.channel.send(message.author.mention ,embed=embed)
+                    return await message.author.add_roles(discord.utils.get(message.guild.roles, id=int(dlswmdrole)))
+                else:
+                    embed3 = discord.Embed(title=f'{name} 인증 시스템', description=f'매세지가 틀렸습니다. 인증 시스템이 종료되었습니다. 다시 시도해주세요.', color=0xFF0000)
+                    return await message.channel.send(embed=embed3)                        
 
         except Exception as errorcode:
                 error = client.get_channel(int(channelid)) 
@@ -150,7 +170,7 @@ async def on_message(message):
                 embed2.add_field(name="오류 코드", value=errorcode, inline=False)  
                 await error.send(embed=embed2)
                 embed3 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-                await message.channel.send(embed=embed3)   
+                return await message.channel.send(embed=embed3)   
 
     if message.content.startswith("!청소"):
         try:
@@ -176,7 +196,7 @@ async def on_message(message):
                     embed.add_field(name="청소 안내", value=f"매세지 {content}개를 제거하였습니다.", inline=False)  
                     embed.set_footer(text=footer)
                     embed.set_thumbnail(url=icon)  
-                    await message.author.send(embed=embed)
+                    return await message.author.send(embed=embed)
 
         except Exception as errorcode:
             error = client.get_channel(int(channelid)) 
@@ -187,18 +207,7 @@ async def on_message(message):
             embed2.add_field(name="오류 코드", value=errorcode, inline=False)  
             await error.send(embed=embed2)
             embed3 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-            await message.channel.send(embed=embed3)                                                      
-
-        except Exception as errorcode:
-            error = client.get_channel(int(channelid)) 
-            embed2 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-            embed2.add_field(name="사용자", value=message.author, inline=False)    
-            embed2.add_field(name="사용자 아이디", value=message.author.id, inline=False)  
-            embed2.add_field(name="명령어", value=message.content, inline=False)      
-            embed2.add_field(name="오류 코드", value=errorcode, inline=False)  
-            await error.send(embed=embed2)
-            embed3 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-            await message.channel.send(embed=embed3)                
+            return await message.channel.send(embed=embed3)                                                      
 
     if message.content.startswith("!핑"):
         try:
@@ -209,7 +218,7 @@ async def on_message(message):
                 return
             else:
                 embed = discord.Embed(title=f'{name}', description=f'퐁 !', color=0x0000FF)
-                await message.channel.send(embed=embed)
+                return await message.channel.send(embed=embed)
 
         except Exception as errorcode:
             error = client.get_channel(int(channelid)) 
@@ -220,8 +229,10 @@ async def on_message(message):
             embed2.add_field(name="오류 코드", value=errorcode, inline=False)  
             await error.send(embed=embed2)
             embed3 = discord.Embed(title=f'{name}', description=f'오류가 발생하였습니다.', color=0xFF0000)
-            await message.channel.send(embed=embed3)
+            return await message.channel.send(embed=embed3)
 
 
 
 client.run(token) # token 
+
+
