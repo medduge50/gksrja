a245ec911f25b22718defc5f42472ab188a8f851
medduge50 <69568693+medduge50@users.noreply.github.com>
2021-10-28 16:03:05 +0900

    Update botclient.py

--- botclient.py
+++ botclient.py
@@ -6,6 +6,5 @@ handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(('', 5959), handler) as httpd:
   ip = requests.get('https://ipinfo.io/ip').text
-  print(ip)
-  print('포트 5959으로 사이트가 생성되었습니다.')
+  print(f'포트 5959으로 사이트가 생성되었습니다. {ip}')
   httpd.serve_forever()
